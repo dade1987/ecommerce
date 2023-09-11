84c264bdabca8295ffd4a1385e51bfcb697d7f42
Davide Cavallini <davidecavallini1987@gmail.com>
2023-09-11 16:27:38 +0200

    tasto con route('categories.products.index' nella view

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
     @foreach ($categories as $category)
-        <li><a href="{{ route('categories.products', ['category' => $category]) }}">{{ $category->name }}</a></li>
+        <li><a href="{{ route('categories.products.index', ['category' => $category]) }}">{{ $category->name }}</a></li>
     @endforeach
 </x-app-layout>
