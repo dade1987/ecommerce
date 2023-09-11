819ea5b09e54005ef67ac824befcdcfeeee1ba44
Davide Cavallini <davidecavallini1987@gmail.com>
2023-09-11 14:20:51 +0200

    aggiunti prodotti con relazione polimorfica molti a molti

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,8 @@ use Awcodes\Curator\Models\Media;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Category extends Model
 {
@@ -16,5 +18,19 @@ class Category extends Model
     public function featuredImage(): BelongsTo
     {
         return $this->belongsTo(Media::class, 'featured_image_id', 'id');
+    }
+
+    public function products(): MorphToMany
+    {
+        $pivot_class = ProductMorph::class;
+        $pivot = app($pivot_class);
+        $pivot_table = $pivot->getTable();
+        $pivot_fields = $pivot->getFillable();
+
+
+        return $this->morphToMany(Product::class, 'model', $pivot_table)
+            ->using($pivot_class)
+            ->withPivot($pivot_fields)
+            ->withTimestamps();
     }
 }
