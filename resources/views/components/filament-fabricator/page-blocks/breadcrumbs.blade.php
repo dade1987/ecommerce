69654c8939ed1960e0a06ef5c0b090930954d74b
Davide Cavallini <davidecavallini1987@gmail.com>
2023-09-12 19:02:05 +0200

    breadcrumbs block

--- resources/views/components/filament-fabricator/page-blocks/breadcrumbs.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/breadcrumbs.blade.php
@@ -3,9 +3,7 @@
 <section>
     <div class="container py-3 mx-auto grid md:grid-cols-2 md:gap-2 sm:grid-cols-1 sm:gap-1">
         <div class="text-center py-3 mx-auto">
-            <div class="text-center">
-                <x-filament::breadcrumbs :breadcrumbs="$breadcrumbs" />
-            </div>
+            <x-filament::breadcrumbs :breadcrumbs="$breadcrumbs" />
         </div>
     </div>
 </section>
