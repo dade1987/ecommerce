8e369d7b5841d01e4bb134098f972a7514e01c13
Margherita Breda <marghi8210@gmail.com>
2023-09-28 19:12:43 +0200

    Blocco di feature-one e pagina home

---
+++ resources/views/components/filament-fabricator/page-blocks/feature-one.blade.php
@@ -0,0 +1,128 @@
+<section class="dark:bg-gray-800 dark:text-gray-100">
+    <div class="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+        <div>
+            <h2 class="text-3xl font-bold tracki text-center sm:text-5xl dark:text-gray-50">{{ $textOne }}</h2>
+            <p class="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">{{ $textTwo }}</p>
+        </div>
+        <div class="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+            <div>
+                <h3 class="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">{{ $textThree }}</h3>
+                <p class="mt-3 text-lg dark:text-gray-400">{{ $textFour }}</p>
+                <div class="mt-12 space-y-12">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div
+                                class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-orange-400 dark:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor" class="w-7 h-7">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M5 13l4 4L19 7"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h4 class="text-lg font-medium leadi dark:text-gray-50">{{ $textFive }}</h4>
+                            <p class="mt-2 dark:text-gray-400">{{ $textSix }}</p>
+                        </div>
+                    </div>
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div
+                                class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-orange-400 dark:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor" class="w-7 h-7">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M5 13l4 4L19 7"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h4 class="text-lg font-medium leadi dark:text-gray-50">{{ $textSeven }}</h4>
+                            <p class="mt-2 dark:text-gray-400">{{ $textEight }}</p>
+                        </div>
+                    </div>
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div
+                                class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-orange-400 dark:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor" class="w-7 h-7">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M5 13l4 4L19 7"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h4 class="text-lg font-medium leadi dark:text-gray-50">{{ $textNine }}</h4>
+                            <p class="mt-2 dark:text-gray-400">{{ $textTen }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div aria-hidden="true" class="mt-10 lg:mt-0">
+                <img src="{{ $linkOne }}" alt="" class="mx-auto rounded-lg shadow-lg dark:bg-gray-500">
+            </div>
+        </div>
+        <div>
+            <div class="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                <div class="lg:col-start-2">
+                    <h3 class="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">{{ $textEleven }}</h3>
+                    <p class="mt-3 text-lg dark:text-gray-400">{{ $textTwelve }}</p>
+                    <div class="mt-12 space-y-12">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <div
+                                    class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-orange-400 dark:text-gray-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" class="w-7 h-7">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M5 13l4 4L19 7"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="ml-4">
+                                <h4 class="text-lg font-medium leadi dark:text-gray-50">{{ $textThirteen }}</h4>
+                                <p class="mt-2 dark:text-gray-400">{{ $textFourteen }}</p>
+                            </div>
+                        </div>
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <div
+                                    class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-orange-400 dark:text-gray-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" class="w-7 h-7">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M5 13l4 4L19 7"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="ml-4">
+                                <h4 class="text-lg font-medium leadi dark:text-gray-50">{{ $textFifteen }}</h4>
+                                <p class="mt-2 dark:text-gray-400">{{ $textSixteen }}</p>
+                            </div>
+                        </div>
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <div
+                                    class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-orange-400 dark:text-gray-900">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" class="w-7 h-7">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M5 13l4 4L19 7"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div class="ml-4">
+                                <h4 class="text-lg font-medium leadi dark:text-gray-50">{{ $textSeventeen }}</h4>
+                                <p class="mt-2 dark:text-gray-400">{{ $textEighteen }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
+                    <img src="{{ $linkTwo }}" alt="" class="mx-auto rounded-lg shadow-lg dark:bg-gray-500">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
