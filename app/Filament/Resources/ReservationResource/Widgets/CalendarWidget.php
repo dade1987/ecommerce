cf874e82d16587a1f0250a74a9e2fc7dcc2002c2
Margherita Breda <marghi8210@gmail.com>
2023-10-06 18:32:25 +0200

    aggiunto calendario con azione create

--- app/Filament/Resources/ReservationResource/Widgets/CalendarWidget.php
+++ app/Filament/Resources/ReservationResource/Widgets/CalendarWidget.php
@@ -2,12 +2,18 @@
 
 namespace App\Filament\Resources\ReservationResource\Widgets;
 
+use App\Models\Reservation;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DateTimePicker;
 use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
 
 
 class CalendarWidget extends FullCalendarWidget
 {
-     /**
+
+    public Model | string | null $model = Reservation::class;
+    /**
      * FullCalendar will call this function whenever it needs new event data.
      * This is triggered when the user clicks prev/next or switches views on the calendar.
      */
@@ -16,6 +22,32 @@ class CalendarWidget extends FullCalendarWidget
         // You can use $fetchInfo to filter events by date.
         // This method should return an array of event-like objects. See: https://github.com/saade/filament-fullcalendar/blob/3.x/#returning-events
         // You can also return an array of EventData objects. See: https://github.com/saade/filament-fullcalendar/blob/3.x/#the-eventdata-class
-        return [];
+        return Reservation::query()
+            //->where('starts_at', '>=', $fetchInfo['start'])
+            //->where('ends_at', '<=', $fetchInfo['end'])
+            ->get()
+            ->map(
+                fn (Reservation $event) => [
+                    'title' => $event->name,
+                    'start' => $event->date_time,
+                    // 'end' => $event->ends_at,
+                    //'url' => EventResource::getUrl(name: 'view', parameters: ['record' => $event]),
+                    //'shouldOpenUrlInNewTab' => true
+                ]
+            )
+            ->all();
+    }
+    public function getFormSchema(): array
+    {
+        return [
+            TextInput::make('name'),
+
+            // Forms\Components\Grid::make()
+            //->schema([
+            DateTimePicker::make('date_time'),
+
+            // Forms\Components\DateTimePicker::make('ends_at'),
+            // ]),
+        ];
     }
 }
