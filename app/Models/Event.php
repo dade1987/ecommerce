46a41e16f8c4d1e6ffc0b53e474730499549aac6
Margherita Breda <marghi8210@gmail.com>
2024-02-13 15:18:14 +0100

    form calendario

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
-    protected $fillable = ['starts_at', 'ends_at'];
+    protected $fillable = ['starts_at', 'ends_at', 'name'];
 }
