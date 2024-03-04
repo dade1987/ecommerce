de50e208275a8c1bcdfc943136a24148fece0336
Margherita Breda <marghi8210@gmail.com>
2024-03-04 18:31:59 +0100

    fatto blocco testimonial

---
+++ app/Filament/Fabricator/PageBlocks/Testimonial.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Builder\Block;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class Testimonial extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('testimonial')
+            ->schema([
+                TextInput::make('logo'),
+                TextInput::make('title'),
+                TextInput::make('subtitle'),
+                TextInput::make('text'),
+            ]);
+    }
+
+    public static function mutateData(array $data): array
+    {
+        $data['logo'] = url('images/' . $data['logo']);
+        return $data;
+    }
+}
