2d93937559933975b6c16598fbdccc853b7e2fcb
Margherita Breda <marghi8210@gmail.com>
2024-04-02 17:17:35 +0200

    migration

--- app/Models/Allergen.php
+++ app/Models/Allergen.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Allergen extends Model
 {
     use HasFactory;
+
     protected $fillable = ['name'];
 }
--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Digikraaft\ReviewRating\Traits\HasReviewRating;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
     use HasReviewRating;
     use HasFactory;
+
     protected $fillable = ['name', 'price_range', 'phone_number', 'email', 'website'];
 }
