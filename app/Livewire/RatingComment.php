701895186afe05b598006c3d4a9b823547e54b77
Margherita Breda <marghi8210@gmail.com>
2024-04-03 15:01:58 +0200

    componenti livewire

--- app/Livewire/RatingComment.php
+++ app/Livewire/RatingComment.php
@@ -2,15 +2,15 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
 use Digikraaft\ReviewRating\Models\Review;
+use Livewire\Component;
 
 class RatingComment extends Component
 {
     public Review $review;
+
     public function mount(int $review_id)
     {
-        
         $this->review = Review::find($review_id);
     }
 
