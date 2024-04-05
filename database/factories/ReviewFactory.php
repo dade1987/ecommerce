fb6f910fb10445b1e36e9b2e71a0f179d718ee94
Margherita Breda <marghi8210@gmail.com>
2024-04-05 11:30:41 +0200

    fatti componenti livewire reviewfactory e ratingcomment

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Restaurant;
+use Digikraaft\ReviewRating\Models\Review;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence,
+            'review' => fake()->paragraph,
+            'rating' => fake()->numberBetween(1, 5), // Genera un punteggio casuale compreso tra 1 e 5
+            'author_type' => User::class,
+            'author_id' => User::factory()->create()->id,
+            'model_type' => Restaurant::class,
+            'model_id' => Restaurant::factory()->create()->id,
+            
+        ];
+    }
+}
