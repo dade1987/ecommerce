f151f7f5717bfa7737b3bb5a17f33b99f4b2a0e7
dadeit1987 <davidecavallini1987@gmail.com>
2024-04-05 11:40:17 +0200

    Add Review model and update review_model configuration

---
+++ app/Models/Review.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Digikraaft\ReviewRating\Models\Review as Digikraaft;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Review extends Digikraaft
+{
+    use HasFactory;
+}
