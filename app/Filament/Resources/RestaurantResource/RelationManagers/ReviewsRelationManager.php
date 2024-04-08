8349d70b8d6c3dc7525bfe29ed8ee07fa6694ba2
Margherita Breda <marghi8210@gmail.com>
2024-04-08 18:31:36 +0200

    Update PHP-CS-Fixer rules and add RestaurantResource

---
+++ app/Filament/Resources/RestaurantResource/RelationManagers/ReviewsRelationManager.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources\RestaurantResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ReviewsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'reviews';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('review')
+                    ->required(),
+                Forms\Components\Select::make('rating')
+                    ->options([
+                        '1' => '1',
+                        '2' => '2',
+                        '3' => '3',
+                        '4' => '4',
+                        '5' => '5',
+                    ])->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('rating'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
