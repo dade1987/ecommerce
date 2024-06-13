b0e9d4076c0875ab3bbac732b150a34f01433772
Margherita Breda <marghi8210@gmail.com>
2024-06-13 19:02:26 +0200

    time line

--- resources/views/components/filament-fabricator/page-blocks/time-line.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/time-line.blade.php
@@ -3,14 +3,13 @@
 <!-- Sezione con immagine -->
 <!-- component -->
 <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
-<link rel="stylesheet"
-    href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
 
 <main class="profile-page">
     <section class="relative block h-500-px ">
         <div class="absolute top-0 w-full h-full bg-center bg-cover"
             style="
-            background-image:url('{{ url('images/appartamenti_1.jpg') }}');
+            background-image:url('{{ $background }}');
           ">
             <span id="blackOverlay"></span>
         </div>
@@ -22,7 +21,7 @@
             </svg>
         </div>
     </section>
-    
+
 </main>
 
 
@@ -30,56 +29,41 @@
 <!-- Timeline -->
 <section>
     <div class="relative wrap overflow-hidden p-10 h-full">
-        <div class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border" style="left: 50%"></div>
+        <div class="border-2-2 absolute border-opacity-20 border-amber-200 h-full border" style="left: 50%"></div>
 
-        <!-- Timeline destra -->
+@php
+ $index=1   
+@endphp
+        @foreach ($cards as $item)
+            
+            <!-- Timeline destra -->
         <div class="mb-8 flex justify-between items-center w-full right-timeline">
             <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
+            <div class="z-20 flex items-center order-1 bg-lime-600 shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">{{ $index }}</h1>
             </div>
-            <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-gray-800 text-xl">{{ $text }}</h3>
-                <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{{ $textTwo }}</p>
+            <div class="order-1 bg-lime-500 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-3 font-bold text-gray-800 text-xl">{{ $item['title_left'] }}</h3>
+                <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{{ $item['text_left'] }}</p>
             </div>
         </div>
 
         <!-- Timeline sinistra -->
         <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
             <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto text-white font-semibold text-lg">2</h1>
+            <div class="z-20 flex items-center order-1 bg-lime-600 shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto text-white font-semibold text-lg">{{ $index + 1 }}</h1>
             </div>
-            <div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-white text-xl">{{ $textThree }}</h3>
-                <p class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                    {{ $textFour }}</p>
+            <div class="order-1 bg-amber-200 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-3 font-bold text-grey-800 text-xl">{{ $item['title_right'] }}</h3>
+                <p class="text-sm font-medium leading-snug tracking-wide text-grey-800 text-opacity-100">
+                    {{ $item['text_right'] }}</p>
             </div>
-        </div>
+        </div> 
+        @php
+        $index+=2
+        @endphp @endforeach
 
-        <!-- Timeline destra -->
-        <div class="mb-8 flex justify-between items-center w-full right-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
-            </div>
-            <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-gray-800 text-xl">{{ $textFive }}</h3>
-                <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{{ $textSix }}</p>
-            </div>
-        </div>
-
-        <!-- Timeline sinistra -->
-        <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-            <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto text-white font-semibold text-lg">4</h1>
-            </div>
-            <div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-white text-xl">{{ $textSeven }}</h3>
-                <p class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                    {{ $textEight }}</p>
-            </div>
-        </div>
     </div>
+
 </section>
