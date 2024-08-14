ad62b095d21cf79f819a85527facc0901b81ccc0
dadeit1987 <davidecavallini1987@gmail.com>
2024-08-14 09:45:34 +0200

    ai799

--- app/Filament/Fabricator/PageBlocks/Sidebar.php
+++ app/Filament/Fabricator/PageBlocks/Sidebar.php
@@ -3,8 +3,8 @@
 namespace App\Filament\Fabricator\PageBlocks;
 
 use App\Models\Menu;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\TextInput;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class Sidebar extends PageBlock
@@ -20,7 +20,7 @@ class Sidebar extends PageBlock
     public static function mutateData(array $data): array
     {
         $data['items'] = Menu::firstWhere('name', $data['name'])->items;
-        
+
         return $data;
     }
 }
