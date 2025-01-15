e69e8111406467bb48396a9661df30f32c99210d
dadeit1987 <davidecavallini1987@gmail.com>
2025-01-15 08:39:51 +0100

    Enhance layout templates by adding Google Analytics tracking code and improving structure. Updated app.blade.php and guest.blade.php to include Google tag manager scripts for better analytics tracking.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,60 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-       
-        
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        @livewireStyles
-        @filamentStyles
-    </head>
-    <body class="font-sans antialiased">
-        <div class="h-full bg-gray-100">
-           {{-- @include('layouts.navigation')--}}
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-D7G8J1GF0M"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-            <!-- Page Content -->
-            <main class="h-full">
-                {{ $slot }}
-            </main>
-        </div>
-       
-        @filamentScripts
-        @livewireScriptConfig
-    </body>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-D7G8J1GF0M');
+    </script>
+
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+    @filamentStyles
+</head>
+
+<body class="font-sans antialiased">
+    <div class="h-full bg-gray-100">
+        {{-- @include('layouts.navigation') --}}
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main class="h-full">
+            {{ $slot }}
+        </main>
+    </div>
+
+    @filamentScripts
+    @livewireScriptConfig
+</body>
+
 </html>
