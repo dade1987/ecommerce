cb5c8d7443b2295ef24a24eb4ee5472fd684fb33
dadeit1987 <davidecavallini1987@gmail.com>
2025-02-07 09:30:39 +0100

    Swap meta tag comment states in app layout
    - Uncomment title meta tag
    - Comment out description and robots meta tags
    - Maintain layout template structure while adjusting meta tag visibility

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-   {{-- <!-- Title -->
+   <!-- Title -->
     <title>@yield('title', config('metatag.default.title'))</title>
 
-    <!-- Meta Description -->
+    {{-- <!-- Meta Description -->
     <meta name="description" content="@yield('meta-description', config('metatag.default.description'))">
 
     <!-- Meta Robots -->
