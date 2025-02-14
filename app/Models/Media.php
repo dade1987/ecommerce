bacdb358042595b3dc7e5560bae149ae01e527be
dadeit1987 <davidecavallini1987@gmail.com>
2025-02-14 14:45:53 +0100

    Remove HasTeams trait from Media model

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use App\Models\Traits\HasTeams;
 use Awcodes\Curator\Models\Media as ModelsMedia;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Media extends ModelsMedia
 {
     use HasFactory;
 
-    use HasTeams;
+    //use HasTeams;
 }
