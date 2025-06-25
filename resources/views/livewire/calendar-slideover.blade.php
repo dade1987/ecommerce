bcd4999b5487be8bbd4bb01c7298cc38000e76e0
Davide Cavallini <davidecavallini1987@gmail.com>
2025-06-25 09:04:38 +0200

    slideover calendar

--- resources/views/livewire/calendar-slideover.blade.php
+++ resources/views/livewire/calendar-slideover.blade.php
@@ -5,6 +5,8 @@
 
     <div class="p-4">
         <p class="text-sm text-gray-500 mb-4">Clicchi sopra ad un orario per effettuare una prenotazione.</p>
-        @livewire(App\Filament\Resources\ReservationResource\Widgets\CalendarWidget::class)
+        <div class="min-h-[80vh]">
+            @livewire(App\Filament\Resources\ReservationResource\Widgets\CalendarWidget::class)
+        </div>
     </div>
 </x-filament::modal>
