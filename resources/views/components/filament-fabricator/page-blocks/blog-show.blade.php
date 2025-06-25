1504e323bad4eddf4f69aa3a860180585bc40283
Davide Cavallini <davidecavallini1987@gmail.com>
2025-06-25 09:12:05 +0200

    open calendar button blog

--- resources/views/components/filament-fabricator/page-blocks/blog-show.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/blog-show.blade.php
@@ -92,14 +92,15 @@
                 </div>
             </div>
 
-            <!-- Share Button -->
-            <div class="text-center mt-10">
+            <!-- Share Buttons -->
+            <div class="text-center mt-10 flex justify-center items-center space-x-4">
                 <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(request()->fullUrl()) }}&title={{ urlencode($row->title) }}&summary={{ urlencode(strip_tags($row->content)) }}" 
                    target="_blank" 
                    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                     Condividi su Linkedin
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
+                @livewire('open-calendar-button')
             </div>
         </div>
     </div>
