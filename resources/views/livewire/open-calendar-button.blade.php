0d91824a83a4724e14be38dcdf68b520331807c7
Davide Cavallini <davidecavallini1987@gmail.com>
2025-06-25 09:14:34 +0200

    open calendar button blog

--- resources/views/livewire/open-calendar-button.blade.php
+++ resources/views/livewire/open-calendar-button.blade.php
@@ -1,6 +1,6 @@
 <button
     wire:click="trigger"
-    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
+    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-orange-600 rounded-lg hover:bg-orange-700 focus:ring-4 focus:ring-orange-300 dark:focus:ring-orange-800"
 >
     Prenota Demo
 </button> 
