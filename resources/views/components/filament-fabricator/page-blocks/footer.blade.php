5f9e3b94a6cda9ba22cf2be63f2392072a6247be
Davide Cavallini <davidecavallini1987@gmail.com>
2025-06-26 11:41:40 +0200

    div footer

--- resources/views/components/filament-fabricator/page-blocks/footer.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/footer.blade.php
@@ -4,68 +4,57 @@
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
-            <div class="mb-6 md:mb-0">
-                <address class="not-italic text-sm text-gray-900 dark:text-white">
-                    <p>DAVIDE CAVALLINI</p>
-                    <p>via del Musonetto 4 - Noale VE - Italy</p>
-                    <p>contatto: +39 320 4206795</p>
-                    <p>P.IVA IT04914550274</p>
-                    <p>C.F.: CVLDVD87M23L736P</p>
-                </address>
-            </div>
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                    <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
-                            <a href="https://cavalliniservice.com/" class="hover:underline">Cavallini Service</a>
-                        </li>
-                        <li>
-                            <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                    <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
-                            <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
-                        </li>
-                        <li>
-                            <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                    <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                        <li class="mb-4">
-                            <a href="https://www.iubenda.com/privacy-policy/75773543" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe hover:underline" title="Privacy Policy">Privacy Policy</a>
-                            <script type="text/javascript">
-                                (function(w, d) {
-                                    var loader = function() {
-                                        var s = d.createElement("script"),
-                                            tag = d.getElementsByTagName("script")[0];
-                                        s.src = "https://cdn.iubenda.com/iubenda.js";
-                                        tag.parentNode.insertBefore(s, tag);
-                                    };
-                                    if (w.addEventListener) {
-                                        w.addEventListener("load", loader, false);
-                                    } else if (w.attachEvent) {
-                                        w.attachEvent("onload", loader);
-                                    } else {
-                                        w.onload = loader;
-                                    }
-                                })(window, document);
-                            </script>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <div class="mb-6 md:mb-0">
+              <address class="not-italic text-sm text-gray-900 dark:text-white">
+                  <p>DAVIDE CAVALLINI</p>
+                  <p>via del Musonetto 4 - Noale VE - Italy</p>
+                  <p>contatto: +39 320 4206795</p>
+                  <p>P.IVA IT04914550274</p>
+                  <p>C.F.: CVLDVD87M23L736P</p>
+              </address>
+          </div>
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://cavalliniservice.com/" class="hover:underline">Cavallini Service</a>
+                      </li>
+                      <li>
+                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                      </li>
+                      <li>
+                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <a href="https://www.iubenda.com/privacy-policy/75773543" class="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe hover:underline" title="Privacy Policy">Privacy Policy</a>
+                          <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+                      </li>
+                      <li>
+                          <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <div class="sm:flex sm:items-center sm:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://cavalliniservice.com/" class="hover:underline">CavalliniService.com™</a>. All Rights Reserved.
+          </span>
+          
+      </div>
     </div>
 </footer>
