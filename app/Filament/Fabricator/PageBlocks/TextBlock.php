55aee22aa8697967d26498e28ef04b267a41be5c
Davide Cavallini <davidecavallini1987@gmail.com>
2025-06-30 16:31:32 +0200

    colore testo

--- app/Filament/Fabricator/PageBlocks/TextBlock.php
+++ app/Filament/Fabricator/PageBlocks/TextBlock.php
@@ -6,9 +6,6 @@ use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\RichEditor;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 use Illuminate\Contracts\View\View;
-use Filament\Forms\Components\Actions\Action;
-use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\Grid;
 
 class TextBlock extends PageBlock
 {
@@ -17,27 +14,6 @@ class TextBlock extends PageBlock
         return Block::make('text-block')
             ->label('Blocco di Testo')
             ->schema([
-                Grid::make(2)->schema([
-                    ColorPicker::make('textColor')
-                        ->label('Colore del Testo')
-                        ->reactive()
-                        ->afterStateUpdated(static function ($state, $livewire, $get, $set) {
-                            // Questo serve per far aggiornare il componente, ma la logica è in Alpine
-                        })
-                        ->extraAttributes([
-                            'x-on:change' => '
-                                const richEditor = document.getElementById(\'content\').closest(\'.fi-fo-rich-editor\').__x;
-                                richEditor.execute(
-                                    (view) => {
-                                        view.dispatch(view.state.tr.setSelection(view.state.selection))
-                                    },
-                                    (view) => {
-                                        view.chain().focus().setColor($event.target.value).run()
-                                    }
-                                );
-                            ',
-                        ]),
-                ]),
                 RichEditor::make('content')
                     ->label('Contenuto')
                     ->required()
@@ -55,7 +31,6 @@ class TextBlock extends PageBlock
                         'alignCenter',
                         'alignRight',
                     ])
-                    ->extraInputAttributes(['style' => 'min-height: 250px;', 'id' => 'content'])
                     ->columnSpanFull(),
             ]);
     }
