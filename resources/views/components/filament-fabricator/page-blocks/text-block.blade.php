6db82a624c80c596e5e0ef33f549f15b578a7457
Davide Cavallini <davidecavallini1987@gmail.com>
2025-06-30 19:15:42 +0200

    modifiche layout

--- resources/views/components/filament-fabricator/page-blocks/text-block.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/text-block.blade.php
@@ -1,6 +1,6 @@
 @aware(['page'])
 <div class="mx-auto max-w-6xl p-6">
-    <div class="text-2xl">
+    <div class="text-xl">
         {!! preg_replace('/WhatsApp/', '<span style="color: green;">Whatsapp</span>', $content, 1) !!}
     </div>
 </div> 
