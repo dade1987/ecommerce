c036abd06ce2ff260c73975910f7f6b7eeba823a
Davide Cavallini <davidecavallini1987@gmail.com>
2025-07-10 17:43:23 +0200

    log curator

--- app/Services/Curator/LoggingServerFactory.php
+++ app/Services/Curator/LoggingServerFactory.php
@@ -3,28 +3,28 @@
 namespace App\Services\Curator;
 
 use Awcodes\Curator\Glide\Contracts\ServerFactory as ServerFactoryContract;
-use Illuminate\Support\Facades\Log;
-use League\Glide\Responses\SymfonyResponseFactory;
 use League\Glide\Server;
 use League\Glide\ServerFactory;
+use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\Support\Facades\Log;
 
 class LoggingServerFactory implements ServerFactoryContract
 {
     public function getFactory(): ServerFactory|Server
     {
-        $sourcePath = storage_path('app');
-        $sourcePathPrefix = 'public';
-
-        Log::info('Curator source path: ' . $sourcePath . '/' . $sourcePathPrefix);
-
         return ServerFactory::create([
-            'driver' => 'gd',
-            'response' => new SymfonyResponseFactory(app('request')),
-            'source' => $sourcePath,
-            'source_path_prefix' => $sourcePathPrefix,
-            'cache' => storage_path('app'),
-            'cache_path_prefix' => '.cache',
-            'max_image_size' => 2000 * 2000,
+            'driver'          => 'gd',
+            'response'        => new \League\Glide\Responses\SymfonyResponseFactory(app('request')),
+            'source'          => storage_path('app/public'),
+            'cache'           => storage_path('app/.cache'),
+            'max_image_size'  => 2000 * 2000,
+            'before_response' => function (Server $server, ServerRequestInterface $request) {
+                // 'path' è settato da Glide come attribute PSR-7
+                $path     = $request->getAttribute('path');
+                // ottoiene il percorso completo al file sorgente
+                $fullPath = $server->getSourcePath($path);
+                Log::info("Curator serving source file: {$fullPath}");
+            },
         ]);
     }
 } 
