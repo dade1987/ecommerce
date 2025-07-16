700d1c25b498e216876f0dc8d033df26a10ba2d0
Davide Cavallini <davidecavallini1987@gmail.com>
2025-07-16 16:00:17 +0200

    traduzione recensioni

---
+++ resources/lang/en/reviews.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'title' => 'What People Say About Us',
+    'reviews_rating' => 'Reviews & Rating',
+    'reviews_only' => 'Reviews',
+    'rating_only' => 'Rating',
+    'view_reply' => 'View & Reply',
+    'giuseppe_longobardi' => [
+        'name' => 'Giuseppe Longobardi',
+        'content' => 'I have worked with Davide in various contexts, he has proven to be a highly competent professional with great problem-solving skills and lateral thinking. I appreciated his ideas on development and code security, anyone would be pleased to have him on their team.',
+        'date' => 'Nov 01, 2023'
+    ],
+    'fernando_curzi' => [
+        'name' => 'Fernando Curzi',
+        'content' => 'A versatile, creative full-stack developer who is quite fast in writing code, with excellent debugging skills and web page analysis written mainly in JavaScript. Good knowledge of computer security and general networking concepts and excellent for pentesting insidious vulnerabilities that essentially reside within web applications.',
+        'date' => 'Apr 16, 2024'
+    ],
+    'flavio_paolini' => [
+        'name' => 'Flavio Paolini',
+        'content' => 'Davide is an excellent professional in his field, with great communication skills. Exchanging information in the field of Web App Penetration Testing is always a great pleasure.',
+        'date' => 'Apr 09, 2024'
+    ],
+    'massimiliano_brolli' => [
+        'name' => 'Massimiliano Brolli',
+        'content' => 'In the context of activities related to the Red Hot Cyber community, Davide immediately proved to be a valuable resource, capable of bringing energy with an always positive attitude aimed at sharing information. Excellent web application developer.',
+        'date' => 'Nov 01, 2023'
+    ]
+]; 
---
+++ resources/lang/it/reviews.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'title' => 'Cosa Dicono di Noi',
+    'reviews_rating' => 'Recensioni & Rating',
+    'reviews_only' => 'Recensioni',
+    'rating_only' => 'Rating',
+    'view_reply' => 'View & Reply',
+    'giuseppe_longobardi' => [
+        'name' => 'Giuseppe Longobardi',
+        'content' => 'Ho lavorato con Davide in diversi contesti, si è dimostrato un professionista altamente competente con una grande capacità di problem solving e pensiero laterale. Ho apprezzato le sue idee sul fronte sviluppo e code security, a tutti farebbe piacere averlo nel proprio team.',
+        'date' => 'Nov 01, 2023'
+    ],
+    'fernando_curzi' => [
+        'name' => 'Fernando Curzi',
+        'content' => 'Uno sviluppatore front-end e back-end versatile, creativo e abbastanza veloce nella scrittura di codice, abilità di debug e di analisi di pagine web scritte maggiormente in JavaScript, buona conoscenza di nozioni, concetti e tematiche di sicurezza informatica e networking in generale e ottima per il pentesting di vulnerabilità insidiose che sostanzialmente risiedono all\'interno delle web application.',
+        'date' => 'Apr 16, 2024'
+    ],
+    'flavio_paolini' => [
+        'name' => 'Flavio Paolini',
+        'content' => 'Davide è un professionista eccellente sul suo campo, con ottime capacità comunicative. Scambiarsi informazioni in ambito Penetration Test Web App è sempre un grande piacere.',
+        'date' => 'Apr 09, 2024'
+    ],
+    'massimiliano_brolli' => [
+        'name' => 'Massimiliano Brolli',
+        'content' => 'Nell\'ambito delle attività inerenti la community di Red Hot Cyber, Davide si è dimostrato da subito una valida risorsa, capace di portare energia con un atteggiamento sempre positivo mirato alla condivisione delle informazioni. Ottimo sviluppatore di applicazioni web.',
+        'date' => 'Nov 01, 2023'
+    ]
+]; 
