f8c07523368c66f7afa6fb745256def978347741
Sviluppo Trentaduebit <sviluppo@trentaduebit.it>
2025-10-24 18:05:46 +0200

    tentativo caricamento stili

--- resources/js/enjoy-talk-3d-element.js
+++ resources/js/enjoy-talk-3d-element.js
@@ -16,11 +16,13 @@ import cssText from '../css/app.css?inline'
 
 // Inietta CSS nel documento se non è già presente
 function injectStylesIfNeeded() {
+  console.log('LOG injectStylesIfNeeded')
   if (!document.getElementById('enjoy-talk-3d-styles')) {
     const style = document.createElement('style')
     style.id = 'enjoy-talk-3d-styles'
     style.textContent = cssText
     document.head.appendChild(style)
+    console.log('LOG style added', style, document.getElementById('enjoy-talk-3d-styles'))
   }
 }
 
