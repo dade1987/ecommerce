2c19878c2b5ac1c130a7501eb787510fe76c454a
Sviluppo Trentaduebit <sviluppo@trentaduebit.it>
2025-10-28 12:36:05 +0100

    prenota call tolto modal (DA CAPIRE MOTIVO)

--- resources/views/livewire/calendar-slideover.blade.php
+++ resources/views/livewire/calendar-slideover.blade.php
@@ -6,16 +6,11 @@
         }, 300)
     "
 >
-    <x-filament::modal id="reservation-calendar" slide-over width="screen">
-        <x-slot name="heading">
-            Effettua una prenotazione
-        </x-slot>
-
-        <div class="p-4">
-            <p class="text-sm text-gray-500 mb-4">Clicchi sopra ad un orario per effettuare una prenotazione.</p>
-            <div class="min-h-[80vh]">
-                @livewire(App\Filament\Resources\ReservationResource\Widgets\CalendarWidget::class)
-            </div>
+    <div class="p-4">
+        <h2 class="text-lg font-bold mb-4">Effettua una prenotazione</h2>
+        <p class="text-sm text-gray-500 mb-4">Clicchi sopra ad un orario per effettuare una prenotazione.</p>
+        <div class="min-h-[80vh]">
+            @livewire(App\Filament\Resources\ReservationResource\Widgets\CalendarWidget::class)
         </div>
-    </x-filament::modal>
+    </div>
 </div>
