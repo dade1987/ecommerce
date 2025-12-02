b2caf59dff19657128d0b309c2eef7972b734638
Sviluppo Trentaduebit <sviluppo@trentaduebit.it>
2025-12-02 10:13:26 +0100

    inserita config mongodb

--- config/database.php
+++ config/database.php
@@ -78,6 +78,19 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGODB_URI'),
+            'database' => env('MONGODB_DATABASE', 'avatar3d_rag'),
+            'options' => [
+                'retryWrites' => true,
+                'w' => 'majority',
+            ],
+            'driver_options' => [
+                'allow_invalid_hostname' => false,
+            ],
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
@@ -125,7 +138,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
