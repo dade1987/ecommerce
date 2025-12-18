69206cca1623c41596e060a919e634e9c0521b97
Sviluppo Trentaduebit <sviluppo@trentaduebit.it>
2025-12-18 14:42:10 +0100

    fix componente

--- resources/views/components/filament-fabricator/page-blocks/cavalliniservice.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/cavalliniservice.blade.php
@@ -1,6 +1,7 @@
-<enjoy-talk-3d 
-    team-slug="{{ $teamSlug ?? '' }}"
-    data-calendly-url="{{ $calendlyUrl ?? '' }}"
+<enjoy-talk-3d
+    team-slug="{{ $data['team_slug'] ?? 'cavalliniservice' }}"
+    calendly-url="{{ $data['calendly_url'] ?? '' }}"
+    glb-url=""
 ></enjoy-talk-3d>
 
 <script src="https://demo-avatar2.trentaduebit.it/js/enjoyTalk3D.standalone.js"></script>
